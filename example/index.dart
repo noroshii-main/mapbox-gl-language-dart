12a72c4155b10bd023cd74a9f22e22411297d9fd
Hiroya-W <hyuyukun@gmail.com>
2024-03-31 18:57:28 +0900

    :+1: Change language to ja

--- example/index.dart
+++ example/index.dart
@@ -1,10 +1,11 @@
 import 'package:mapbox_gl_dart/mapbox_gl_dart.dart';
+import 'package:mapbox_gl_language_dart/mapbox_gl_language_dart.dart';
 
 void main() {
   Mapbox.accessToken =
       "pk.eyJ1IjoiYW5kcmVhNjg5IiwiYSI6ImNrNGlsYjhyZDBuYXoza213aWphOGNjZmoifQ.maw_5NsXejG1DoOeOi6hlQ";
 
-  MapboxMap(
+  final map = MapboxMap(
     MapOptions(
       container: 'map',
       style: 'mapbox://styles/mapbox/dark-v10',
@@ -12,4 +13,8 @@ void main() {
       zoom: 12,
     ),
   );
+
+  final language = MapboxLanguage(MapboxLanguageOptions(defaultLanguage: 'ja'));
+
+  map.addControl(language);
 }
