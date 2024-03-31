72efa5a9a61d0be8e0972fc0fd5894838b527c92
Hiroya-W <hyuyukun@gmail.com>
2024-03-31 18:56:56 +0900

    :sparkles: Implemented MapboxLanguage

--- lib/mapbox_gl_language_dart.dart
+++ lib/mapbox_gl_language_dart.dart
@@ -1,3 +1,45 @@
-int calculate() {
-  return 6 * 7;
+// ignore_for_file: implementation_imports
+
+@JS()
+library mapbox_gl_language_dart;
+
+import 'package:js/js.dart';
+import 'package:mapbox_gl_dart/mapbox_gl_dart.dart';
+import 'package:mapbox_gl_dart/src/interop/interop.dart';
+
+class MapboxLanguage extends JsObjectWrapper<MapboxLanguageJsImpl> {
+  factory MapboxLanguage(MapboxLanguageOptions options) =>
+      MapboxLanguage.fromJsObject(MapboxLanguageJsImpl(options.jsObject));
+
+  onAdd(MapboxMap map) => jsObject.onAdd(map.jsObject);
+
+  onRemove() => jsObject.onRemove();
+
+  getDefaultPosition() => jsObject.getDefaultPosition();
+
+  MapboxLanguage.fromJsObject(super.jsObject) : super.fromJsObject();
+}
+
+@JS('MapboxLanguage')
+class MapboxLanguageJsImpl {
+  external factory MapboxLanguageJsImpl(MapboxLanguageOptionsJsImpl options);
+  external onAdd(MapboxMapJsImpl map);
+  external onRemove();
+  external getDefaultPosition();
+}
+
+class MapboxLanguageOptions
+    extends JsObjectWrapper<MapboxLanguageOptionsJsImpl> {
+  factory MapboxLanguageOptions({String? defaultLanguage}) =>
+      MapboxLanguageOptions.fromJsObject(
+          MapboxLanguageOptionsJsImpl(defaultLanguage: defaultLanguage));
+
+  MapboxLanguageOptions.fromJsObject(super.jsObject) : super.fromJsObject();
+}
+
+@JS()
+@anonymous
+class MapboxLanguageOptionsJsImpl {
+  external String get defaultLanguage;
+  external factory MapboxLanguageOptionsJsImpl({String? defaultLanguage});
 }
